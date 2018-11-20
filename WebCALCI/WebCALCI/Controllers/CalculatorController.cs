80d334a9ab1000a66c07953184965267dc6d99c4
GOVINDHARAJU PALANI <apgovinda@gmail.com>
2018-11-20 22:14:16 +0530

    Added WEBCALCI

---
+++ WebCALCI/WebCALCI/Controllers/CalculatorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebCALCI.Models;
+
+namespace WebCALCI.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public static List<Operation> operations = new List<Operation>();
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View(operations);
+        }
+        public ActionResult UpdateOperations(Operation operation)
+        {
+            operation.Result = operation.Num1 + operation.Num2;
+            operations.Add(operation);
+            return View("index", operations);
+        }
+    }
+}
